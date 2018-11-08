5847fed4d61b5e4b184787e594f4b548ad7c632a
Michael Serajnik <ms@mserajnik.at>
2018-11-09 00:50:38 +0100

    Update dependencies

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: {
-    'postcss-preset-env': {
-      // workaround for https://github.com/postcss/postcss-custom-properties/issues/150
-      features: {
-        'custom-properties': false
-      }
-    }
+    'postcss-preset-env': {}
   }
 }
