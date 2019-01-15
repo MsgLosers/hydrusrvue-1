d124a57484947d88fa048382362876ad3af0b141
Michael Serajnik <ms@mserajnik.at>
2019-01-15 18:34:08 +0100

    docs: Update changelog and version numbers for release 4.0.0

--- src/config/index.js
+++ src/config/index.js
@@ -21,7 +21,7 @@ const generateNamespaceColors = environmentVariable => {
 }
 
 export default {
-  version: '3.3.0',
+  version: '4.0.0',
   title: process.env.VUE_APP_TITLE,
   subtitle: process.env.VUE_APP_SUBTITLE,
   useNormalLetterCase: process.env.VUE_APP_USE_NORMAL_LETTER_CASE === 'true',
